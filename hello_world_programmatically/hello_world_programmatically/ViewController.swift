0bcef63cadc314b411a026a7a11a4105d89dfa05
victor <konsolidate@gmail.com>
2015-03-29 12:29:56 -0700

    first working init window progrmatically:
    Test Plan:
    
    Reviewers:
    
    Subscribers:

---
+++ hello_world_programmatically/hello_world_programmatically/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  hello_world_programmatically
+//
+//  Created by Victor Liew on 3/29/15.
+//  Copyright (c) 2015 Victor Liew. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ hello_world_programmatically/hello_world_programmaticallyTests/hello_world_programmaticallyTests.swift
@@ -0,0 +1,36 @@
+//
+//  hello_world_programmaticallyTests.swift
+//  hello_world_programmaticallyTests
+//
+//  Created by Victor Liew on 3/29/15.
+//  Copyright (c) 2015 Victor Liew. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class hello_world_programmaticallyTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
