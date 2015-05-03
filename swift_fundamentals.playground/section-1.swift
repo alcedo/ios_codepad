8b268bc857abfe6f64fcb9eec2f4c696d4de8811
victor <konsolidate@gmail.com>
2015-05-02 22:49:21 -0700

    swift : updated playground
    Test Plan:
    
    Reviewers:
    
    Subscribers:

--- swift_fundamentals.playground/section-1.swift
+++ swift_fundamentals.playground/section-1.swift
@@ -5,3 +5,27 @@ import UIKit
 var str = "Hello, playground"
 
 
+let t:(String) -> String = { (String) in
+    println("hello")
+    return "hello"
+}
+
+
+let q:(String)->String = {s1 in return s1}
+
+q("ha")
+
+func me<T>(name: T) -> T {
+    return name
+}
+
+
+for i in 0...4 {
+   print(i)
+}
+
+
+for x in stride(from:2.2, through:2.9, by:0.3)
+{
+    println (x)
+}
