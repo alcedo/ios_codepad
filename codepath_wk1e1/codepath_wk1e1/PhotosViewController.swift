0edf296c501515c08fef18ffd92c4048814d7067
victor <konsolidate@gmail.com>
2015-05-03 00:18:36 -0700

    pod file: bug fixes
    Test Plan:
    
    Reviewers:
    
    Subscribers:

--- codepath_wk1e1/codepath_wk1e1/PhotosViewController.swift
+++ codepath_wk1e1/codepath_wk1e1/PhotosViewController.swift
@@ -8,10 +8,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class PhotosViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+        
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
